60725771a13b558259e4f453fa2e952d2b7d183b
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2024-01-15 15:13:47 +0100

    Z

---
+++ database/migrations/2014_10_12_000001_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('id_U')->primary();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('userName');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('image')->nullable();
+            $table->string('status');
+            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            // $table->rememberToken();
+            $table->string('role'); 
+            $table->foreign('id_R')->references('id_R')->on('roles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
